65c8b8e7e1ad18a67ed055956ba0e5b09be8dfef
tomzo42 <tom.zohar@hibob.io>
2023-12-13 20:21:02 +0200

    added sidenav

--- projects/ui/src/lib/sidenav/sidenav.component.ts
+++ projects/ui/src/lib/sidenav/sidenav.component.ts
@@ -1,7 +1,5 @@
 import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {RouterLink, RouterLinkActive} from "@angular/router";
-import {NgIf} from "@angular/common";
 
 @Component({
   standalone: true,
@@ -9,7 +7,7 @@ import {NgIf} from "@angular/common";
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatSidenavModule, RouterLinkActive, RouterLink, NgIf]
+  imports: [MatSidenavModule]
 })
 export class SidenavComponent {
 }
